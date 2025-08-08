6d21bdf8ff38fbb4f7b171c7060c242d07f305e1
rihahart <nathriha@gmail.com>
2025-08-08 02:37:36 -0400

    about us changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,8 @@
 
 import React from "react";
 import Hero from "../components/home/Hero";
-import useWindowWidth from "./_utilities/useWindowWidth";
 
 export default function RootHome() {
-  const windowWidth = useWindowWidth();
-
   return (
     <main className="w-full min-h-screen">
       <Hero />
