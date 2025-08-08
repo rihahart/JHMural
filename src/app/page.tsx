2aac8b7d47bdb941769d04acee973a4fafab1507
rihahart <nathriha@gmail.com>
2025-08-08 00:36:33 -0400

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import Hero from "../components/Hero";
+
+export default function RootHome() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+} 
