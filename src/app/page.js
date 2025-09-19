58bd20b5f1ae72b5d79e55e3b1298bd9402a7282
rihahart <nathriha@gmail.com>
2025-09-19 02:43:29 -0400

    Remove MissionSection import and usage from main page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@
 import React from "react";
 import Hero from "../components/home/Hero";
 import CollaborationSection from "../components/home/CollaborationSection";
-import MissionSection from "../components/home/MissionSection";
 import DonationSection from "../components/home/DonationSection";
 import ContentSections from "../components/home/ContentSections";
 
@@ -12,7 +11,6 @@ export default function RootHome() {
     <div className="w-full min-h-screen">
       <Hero />
       <CollaborationSection />
-      <MissionSection />
       <DonationSection />
       <ContentSections />
     </div>
