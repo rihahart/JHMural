1ab4fc453df501eae5a7bde1abca25317bd9451a
Riha Hart <nathriha@gmail.com>
2025-10-25 14:47:01 -0400

    push changes

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import MeetOurFounders from "@/components/JHMuralteam/meetOurFounders";
 import CoFounderInfo from "@/components/JHMuralteam/coFounderInfo";
+import OurWallsOurStories from "@/components/JHMuralteam/ourwallsourstories";
 
 export default function MeetTheTeam() {
   return (
     <div className="flex w-full max-w-[1600px] px-4 sm:px-6 lg:px-8 pt-[var(--spacing-xl)] pb-[var(--spacing-7xl)] flex-col items-center gap-12 lg:gap-20 min-h-screen mx-auto">
-      <MeetOurFounders />
+      <OurWallsOurStories />
       <CoFounderInfo />
     </div>
   );
