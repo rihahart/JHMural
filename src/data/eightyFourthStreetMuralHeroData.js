c85ed9d02c4754c3ee8969ac37d6fdb070605395
Riha Hart <nathriha@gmail.com>
2025-10-29 19:03:07 -0400

    Update 84th Street Mural hero data

--- src/data/eightyFourthStreetMuralHeroData.js
+++ src/data/eightyFourthStreetMuralHeroData.js
@@ -1,5 +1,5 @@
 export const eightyFourthStreetMuralHeroData = {
-  title: "Welcome to Jackson Heights Mural",
+  title: "JH Mural Project Welcomes you to Jackson Heights",
   subtitle: "The mural, located at 84th Street and Roosevelt Avenue, is JH Mural's first project. Painted by artist Noah Bassman, it's a colorful love letter to Jackson Heights, vibrant and full of life just like Jackson Heights itself.",
   image: "/84thStreetMural/Hero Image.png"
 };
