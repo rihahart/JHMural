a6c619f566897e818cf0c412c5f7a9541c414742
Riha Hart <nathriha@gmail.com>
2025-10-29 23:18:29 -0400

    Fix desktop flex column layout in HeroBanner and update spacing

--- src/components/84thStreetMural/HeroBanner.tsx
+++ src/components/84thStreetMural/HeroBanner.tsx
@@ -15,7 +15,7 @@ export default function HeroBanner() {
         <div className="flex flex-col items-center gap-[var(--spacing-2xl)] py-[var(--spacing-2xl)] px-[var(--spacing-lg)]">
           <div className="flex flex-col items-start align-stretch gap-[var(--spacing-2xl)]">
         
-            <h2 className="mobile-heading-3xl-bold text-[var(--color-content-primary)]">{topTitle}</h2>
+            <h2 className="mobile-heading-4xl-bold text-[var(--color-content-primary)]">{topTitle}</h2>
             <h2 className="hero-xs text-[var(--color-content-primary)]">{title}</h2>
         
             <p className="mobile-text-m-semibold text-[var(--color-content-primary)]">{subtitle}</p>
@@ -61,13 +61,11 @@ export default function HeroBanner() {
     return (
       <div className="w-full bg-[var(--color-yellow-500)]">
         <div className="flex items-stretch w-full max-w-[1400px] mx-auto gap-[var(--spacing-6xl)] py-[var(--spacing-6xl)] px-[var(--spacing-6xl)]">
-         
-          
+          <div className="flex flex-col items-start w-1/2 gap-[var(--spacing-2xl)]">
             <h2 className="web-heading-2xl-bold text-[var(--color-content-primary)]">{topTitle}</h2>
             <h2 className="hero-lg text-[var(--color-content-primary)]">{title}</h2>
-            
             <p className="web-text-2xl-regular text-[var(--color-content-primary)]">{subtitle}</p>
-      
+          </div>
           <div className="w-1/2 flex flex-col overflow-hidden">
             <Image 
               src={image} 
@@ -86,10 +84,12 @@ export default function HeroBanner() {
   return (
     <div className="w-full bg-[var(--color-yellow-500)]">
       <div className="flex items-stretch w-full max-w-[1600px] mx-auto gap-[var(--spacing-6xl)] py-[var(--spacing-6xl)] px-[var(--spacing-6xl)]">
+        <div className="flex flex-col items-start w-1/2 gap-[var(--spacing-6xl)]">
           <h2 className="web-heading-2xl-bold text-[var(--color-content-primary)]">{topTitle}</h2>
           <h2 className="hero-2xl text-[var(--color-content-primary)]">{title}</h2>
           <p className="web-text-2xl-regular text-[var(--color-content-primary)]">{subtitle}</p> 
-      <div className="w-1/2 flex flex-col overflow-hidden">
+        </div>
+        <div className="w-1/2 flex flex-col overflow-hidden">
           <Image 
             src={image} 
             alt="Jackson Heights Mural at 84th Street and Roosevelt Avenue" 
