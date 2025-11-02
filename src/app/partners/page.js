0d43afbc1d2c2ab64b13c25278549ad6d505b455
Riha Hart <nathriha@gmail.com>
2025-11-02 16:13:20 -0500

    Fix partners page by removing deleted component imports

--- src/app/partners/page.js
+++ src/app/partners/page.js
@@ -1,20 +1,20 @@
 "use client";
 
 import React from "react";
-import SupportersHero from "@/components/supporters/SupportersHero";
-import OrganizationSupporters from "@/components/supporters/OrganizationSupporters";
 import IndividualSupporters from "@/components/supporters/IndividualSupporters";
-import JoinSupporters from "@/components/supporters/JoinSupporters";
-import GetInvolvedSection from "@/components/supporters/GetInvolvedSection";
 
 export default function Supporters() {
   return (
     <div className="w-full min-h-screen">
-      <SupportersHero />
-      <OrganizationSupporters />
+      <div className="max-w-[1200px] mx-auto px-6 py-16">
+        <h1 className="text-4xl font-bold text-[var(--color-content-primary)] mb-8">
+          Partners & Supporters
+        </h1>
+        <p className="text-lg text-[var(--color-content-secondary)] mb-8">
+          Thank you to all our partners and supporters who make our work possible.
+        </p>
+      </div>
       <IndividualSupporters />
-      <JoinSupporters />
-      <GetInvolvedSection />
     </div>
   );
 }
