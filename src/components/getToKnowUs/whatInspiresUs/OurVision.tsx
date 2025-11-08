afefa2fb829c32828b138ddf6dc82c77eb537842
Riha Hart <nathriha@gmail.com>
2025-11-08 15:41:57 -0500

    Add subtitle to OurVision component

--- src/components/getToKnowUs/whatInspiresUs/OurVision.tsx
+++ src/components/getToKnowUs/whatInspiresUs/OurVision.tsx
@@ -6,13 +6,14 @@ import ourVisionData from "@/data/whatInspiresUs/ourVisionData";
 
 export default function OurVision() {
   const { isMobile, isTablet, isDesktop1440px } = useMobileDetection();
-  const { title, description } = ourVisionData;
+  const { title, description, subtitle } = ourVisionData;
 
   if (isMobile) {
     return (
       <div className="flex flex-col items-start gap-[var(--spacing-xl)]">
         <h1 className="mobile-heading-5xl-bold text-[var(--color-content-primary)]">{title}</h1>
         <p className="mobile-text-m-medium text-[var(--color-content-primary)]">{description}</p>
+        <p className="mobile-text-m-black text-[var(--color-content-primary)]">{subtitle}</p>
       </div>
     );
   }
@@ -21,7 +22,10 @@ export default function OurVision() {
     return (
       <div className="flex items-center align-stretch justify-center gap-[var(--spacing-6xl)]">
         <h1 className="web-heading-4xl-bold text-[var(--color-content-primary)] w-1/2 ">{title}</h1>
-        <p className="web-text-m-medium text-[var(--color-content-primary)] w-1/2">{description}</p>
+        <div className="flex flex-col gap-[var(--spacing-m)] w-1/2">
+          <p className="web-text-m-medium text-[var(--color-content-primary)]">{description}</p>
+          <p className="web-text-m-black text-[var(--color-content-primary)]">{subtitle}</p>
+        </div>
       </div>
     );
   }
@@ -30,7 +34,10 @@ export default function OurVision() {
     return (
       <div className="flex items-center align-stretch justify-center max-w-[1400px] gap-[var(--spacing-8xl)]">
         <h1 className="hero-xs text-[var(--color-content-primary)] w-1/2">{title}</h1>
-        <p className="web-text-xl-medium text-[var(--color-content-primary)] w-1/2">{description}</p>
+        <div className="flex flex-col gap-[var(--spacing-m)] w-1/2">
+          <p className="web-text-xl-medium text-[var(--color-content-primary)]">{description}</p>
+          <p className="web-text-xl-black text-[var(--color-content-primary)]">{subtitle}</p>
+        </div>
       </div>
     );
   }
@@ -38,7 +45,10 @@ export default function OurVision() {
   return (
     <div className="flex justify-center align-stretch items-center max-w-[1600px] gap-[var(--spacing-8xl)]">
       <h1 className="hero-s text-[var(--color-content-primary)] w-1/2">{title}</h1>
-      <p className="web-text-2xl-medium text-[var(--color-content-primary)] w-1/2">{description}</p>
+      <div className="flex flex-col gap-[var(--spacing-m)] w-1/2">
+        <p className="web-text-2xl-medium text-[var(--color-content-primary)]">{description}</p>
+        <p className="web-text-2xl-black text-[var(--color-content-primary)]">{subtitle}</p>
+      </div>
     </div>
   );
 }
--- src/data/whatInspiresUs/ourVisionData.js
+++ src/data/whatInspiresUs/ourVisionData.js
@@ -1,6 +1,7 @@
 export const ourVisionData = {
-  title: "Our mission and vision",
-  description: "This is Jackson Heights. The most diverse community on earth. Here, every blank wall is an opportunity to change the world. That's why we built The Jackson Heights Mural Project. This is our chance to add color to the concrete. To turn blight into beauty. Apathy into pride. And turn walls into windows that show the world what unity looks like. We paint because we believe in this place. Because our children deserve a more colorful future. Because art reminds us who we are and who we can be."
+  title: "Our vision",
+  description: "This is Jackson Heights. The most diverse community on earth. Here, every blank wall is an opportunity to change the world. That's why we built The Jackson Heights Mural Project. This is our chance to add color to the concrete. To turn blight into beauty. Apathy into pride. And turn walls into windows that show the world what unity looks like. We paint because we believe in this place. Because our children deserve a more colorful future. Because art reminds us who we are and who we can be.",
+  subtitle: "Let's cover Jackson Heights in color."
 };
 
 export default ourVisionData;
